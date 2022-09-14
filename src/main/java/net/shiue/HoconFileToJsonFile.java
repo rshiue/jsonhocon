4470fd6213e6ac6d9920cab07f3f623649c69c09
Robert Shiue <robertdx@gmail.com>
2022-09-14 19:38:23 +0800

    Initial commit

---
+++ src/main/java/net/shiue/HoconFileToJsonFile.java
@@ -0,0 +1,13 @@
+package net.shiue;
+import com.typesafe.config.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HoconFileToJsonFile {
+
+    public static void main(String[] args){
+        System.out.println("ok");
+    }
+}
---
+++ src/test/java/net/shiue/JsonFileToHoconFileTest.java
@@ -0,0 +1,20 @@
+package net.shiue;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class JsonFileToHoconFileTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
